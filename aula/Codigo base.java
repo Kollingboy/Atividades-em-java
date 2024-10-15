f4435fdec031cd3bf6eba5a53fb7a71a4a24bc23
João Kolling <156047003+Kollingboy@users.noreply.github.com>
2024-10-15 16:13:02 -0300

    Teste

---
+++ aula/Codigo base.java
@@ -0,0 +1,20 @@
+public class TestedeMesa {
+
+    public class Fatorial {
+        public static void main(String[] args) {
+            int numero = 5;
+            int fatorial = calcularFatorial(numero);
+            System.out.println("O fatorial de " + numero + " é: " + fatorial);
+        }
+    
+        public static int calcularFatorial(int n) {
+            if (n == 0) {
+                return 1;
+            } else {
+                return n * calcularFatorial(n - 1);
+            }
+        }
+    }    
+
+    
+}
