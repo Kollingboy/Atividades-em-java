1d08dcfa7d24a26b2aeb42755f98011ec9d2450f
João Kolling <156047003+Kollingboy@users.noreply.github.com>
2024-10-22 16:09:33 -0300

    Atv 22-10-24 atualizado

---
+++ Atv 22-10-24/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+        Pessoa pessoa1 = new Pessoa();
+        Pessoa pessoa2 = new Pessoa();
+
+        pessoa1.setNome("Kolling");
+        pessoa1.setIdade(19);
+
+        System.out.println("nome: " + pessoa1.getNome() + "\nIdade: " + pessoa1.getIdade());
+
+    }
+
+
+}
